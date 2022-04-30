de1b486256db0f85c62137d8217fe616a62e92f1
marcomanunta <frozenborder@gmail.com>
2022-04-30 08:36:58 +0200

    nested routes and Outlet component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component'
 
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
+
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <Routes>
+    <Route path="/home" element={<Home />}>
+      <Route path="shop" element={<Shop />} />
+    </Route>
+  </Routes>
 )
 
 export default App
